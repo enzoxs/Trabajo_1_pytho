8d5c13c0c2e67784bfe70bef283903b3b4b76eed
Etzio <enzo_002@hotmail.com>
2023-04-17 21:11:55 -0400

    Archivos Actualizados

--- TDA201.py
+++ TDA201.py
@@ -1,30 +1,27 @@
-#ingreso de datos en vatiables
+#ingreso de datos en vatiables #A listo
 import math
 
 a=int(input("Ingrese el numero a :"))
 b=int(input("ingrese el numero b :"))
 c=int(input("Ingrese el numero c :"))
 
-print (a)
-print (b)
-print (c)
 
 #ax^2+bx+c=0
 #b^2-4*a*c
 dis=math.pow(b,2)-(4*a*c)
 
-print(f"El valor de dis es {dis}")
+print(f"El valor de dis es {dis}") #c listo
 
 if(dis>0):
-    print("la cuadrática tiene dos soluciones reales distintas")
-elif(dis==0):
-    print("La solucion cuadratica tiene 2 soluciones reales disponibles")
-elif(dis<0):
-    print("Ninguna de las soluciones son números reales")
-
-if(dis>0):
-    x1 = float((-b+math.sqrt(dis))/math.pow(2,a))
-    x2 = float((-b - math.sqrt(dis)) / math.pow(2, a))
-    print (f"El valor de x1 es {x1}")
+    print("la cuadraﾌ》ica tiene dos soluciones reales distintas")
+    x1 = float((-b + (math.sqrt(dis)))/(2*a))
+    x2 = float((-b - (math.sqrt(dis)))/(2*a))
+    print(f"El valor de x1 es {x1}")
     print(f"El valor de x2 es {x2}")
+
+elif(dis==0):
+    print("La cuadratica tiene una solucion real repetida")
+elif(dis<0):
+    print("Ninguna de las soluciones son nuﾌ［eros reales")
+
 
